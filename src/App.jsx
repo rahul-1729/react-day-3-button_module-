8029618626d105d060c38d11c4dff4570fe74289
rahul-1729 <rk9763981@gmail.com>
2024-06-16 20:52:27 +0530

    button module

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// How to style react components with css
+// ............................
+// (not including externalnframeworks or preprocessors)
+
+// 1.External
+// 2.Modules
+// 3.Inline
+
+import Button from './Button/Button.jsx'
+
+function App() {
+    return (
+      <Button/>
+      );
+}
+
+export default App
